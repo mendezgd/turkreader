98ddddd68b95da15f37986871febf88647a384ed
gmendez <mendezgd88@gmail.com>
2024-03-28 21:46:38 -0300

    fix pag dobles

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,16 @@
-
 const Blog = () => {
-
+    const paginasDobles = [9, 61, 203];
     const imagenes = [];
-    for (let i = 1; i <= 211; i++) {
-        const paddedNumber = String(i).padStart(3, '0');
+
+    for (let i = 1; i <= 211; ) {
+        let paddedNumber = String(i).padStart(3, '0');
+        if (paginasDobles.includes(i)) {
+            const nextPage = String(i + 1).padStart(3, '0');
+            paddedNumber = `${paddedNumber}-${nextPage}`;
+            i += 2;
+        } else {
+            i++;
+        }
         imagenes.push(`20thCB_${paddedNumber}.jpg`);
     }
 
