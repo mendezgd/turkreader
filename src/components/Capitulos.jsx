fd1a56f22c347424ea33605b21d59702eceaf9fd
fattiepoX ... <mendezgd88@gmail.com>
2024-07-16 17:16:18 -0300

    speed of light

--- src/components/Capitulos.jsx
+++ src/components/Capitulos.jsx
@@ -1,22 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UseContext } from './UseContext';
+import Loader from './Loader';
 
 const Capitulos = () => {
-    const { portadas } = useContext(UseContext); 
+    const { portadas } = useContext(UseContext);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (portadas.length > 0) {
+            setLoading(false);
+        }
+    }, [portadas]);
 
     return (
-        <div className="portadas-container">
-            {portadas.map((portada) => (
-                <Link key={portada.capitulo} to={`/Cap${portada.capitulo}`}>
-                    <img
-                        src={portada.url}
-                        alt={`Portada del capítulo ${portada.capitulo}`}
-                        className="portada-img h-96 w-72"
-                    />
-                </Link>
-            ))}
-        </div>
+        <>
+            <h1 className="text-center text-4xl p-5 dark:bg-slate-700 dark:text-white">Capítulos</h1>
+            <section className="flex flex-col items-center dark:bg-slate-800">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-items-center">
+                    {loading ? (
+                        <Loader />
+                    ) : (
+                        portadas.map((portada) => (
+                            <Link key={portada.capitulo} to={`/Cap${portada.capitulo}`}>
+                                <div className="flex flex-col items-center m-4">
+                                    <img
+                                        src={portada.url}
+                                        alt={`Portada del capítulo ${portada.capitulo}`}
+                                        className="portada-img h-96 w-72 rounded"
+                                    />
+                                </div>
+                            </Link>
+                        ))
+                    )}
+                </div>
+            </section>
+        </>
     );
 };
 
